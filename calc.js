d29bb69e23f20e14f32a12d0a1890cebafcb738a
Davidfhk <davidign1688@hotmail.com>
2017-04-30 20:25:30 +0200

    fallo del igual solucionado

--- calc.js
+++ calc.js
@@ -1,19 +1,19 @@
-var cifra = ""
-var acumulado = 0
-var resultado = 0
-var suma = false
-var resta = false
+var cifra = "";
+var acumulado = 0;
+var resultado = 0;
+var suma = false;
+var resta = false;
+
 function number (num){
-	document.getElementById('display').value = cifra = cifra + num
+	document.getElementById('display').value = cifra = cifra + num;
 }
 function sumar (){
 	if (resta == true) {
-		if (cifra == "") {
-			cifra = 0
-		}
+		cifra();
 		acumulado = acumulado - parseInt(cifra);
 		resta = false;
 	}else{
+		cifra();
 		acumulado = acumulado + parseInt(cifra);
 	}
 	
@@ -23,27 +23,36 @@ function sumar (){
 }
 function igual (){
 	if(suma == true){
+		cifra();
 		resultado = acumulado + parseInt(cifra);
-		document.getElementById('display').value = resultado
+		document.getElementById('display').value = resultado;
 	}
 	if(resta == true){
+		cifra();
 		resultado = acumulado - parseInt(cifra);
-		document.getElementById('display').value = resultado
+		document.getElementById('display').value = resultado;
 	}
-	suma = false
-	resta = false
+	acumulado = resultado;
+	cifra = "";
+	suma = false;
+	resta = false;
 }
 function restar (){
 	if (acumulado == 0 || suma == true){
-		if (cifra == "") {
-			cifra = 0
-		}
+		cifra();
 		acumulado = acumulado + parseInt(cifra);
-		suma = false
+		suma = false;
 	}else{
+		cifra();
 		acumulado = acumulado - parseInt(cifra);
 	}
 		cifra = "";
 		document.getElementById('display').value = acumulado;
-		resta = true
+		resta = true;
+}
+
+function cifra () {
+	if (cifra == "") {
+		cifra = 0;
+	}
 }
